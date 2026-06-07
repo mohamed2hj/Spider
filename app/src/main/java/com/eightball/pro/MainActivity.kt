88e5f72a8b842d4a577a10824265056f9b33b9bf
mohamed2hj <yousef.998634@gmail.com>
2026-06-07 19:48:05 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/eightball/pro/MainActivity.kt
@@ -0,0 +1,86 @@
+herepackage com.eightball.pro
+
+import android.app.Activity
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.eightball.pro.service.OverlayService
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_MEDIA_PROJECTION = 1001
+        private const val REQUEST_OVERLAY_PERMISSION = 1002
+    }
+
+    private var isRequestingProjection = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        isRequestingProjection = intent?.getBooleanExtra("request_projection", false) ?: false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    android.net.Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, REQUEST_OVERLAY_PERMISSION)
+            } else {
+                startServiceAndFinish()
+            }
+        } else {
+            startServiceAndFinish()
+        }
+    }
+
+    private fun startServiceAndFinish() {
+        val serviceIntent = Intent(this, OverlayService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent)
+        } else {
+            startService(serviceIntent)
+        }
+
+        if (isRequestingProjection) {
+            requestMediaProjection()
+        } else {
+            finish()
+        }
+    }
+
+    private fun requestMediaProjection() {
+        val projectionManager = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        startActivityForResult(projectionManager.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_OVERLAY_PERMISSION -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                    startServiceAndFinish()
+                } else {
+                    Toast.makeText(this, "مطلوب صلاحية الظهور فوق التطبيقات", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+
+            REQUEST_MEDIA_PROJECTION -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    OverlayService.getInstance()?.onMediaProjectionGranted(resultCode, data)
+                } else {
+                    Toast.makeText(this, "مطلوب صلاحية تصوير الشاشة", Toast.LENGTH_LONG).show()
+                }
+                finish()
+            }
+        }
+    }
+}
